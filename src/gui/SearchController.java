37384a10b40be617fa549b1415c24e3caa936dde
andersthomsen92 <doomknight92@gmail.com>
2023-11-07 10:00:13 +0100

    Initial commit

---
+++ src/gui/SearchController.java
@@ -0,0 +1,34 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class SearchController {
+
+    @FXML
+    private Button btnSearch;
+    @FXML
+    private Button btnStats;
+    @FXML
+    private Label lbl;
+    @FXML
+    private ListView<?> listHistory;
+    @FXML
+    private ListView<?> listWordN;
+    @FXML
+    private ListView<?> listWords;
+    @FXML
+    private ListView<?> listWordsA;
+    @FXML
+    private TextField txtSearch;
+    @FXML
+    void clickBtn(ActionEvent event)
+    {
+    
+    }
+
+}
